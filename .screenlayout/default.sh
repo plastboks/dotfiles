780d5ec6bd3bd71772e27de733c83e851c421bc7
Alexander Skjolden <alex@plastboks.net>
2012-12-21 09:15:57 +0100

    hostname spesific screenlayout sh file

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output VGA-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI-1 --off
+
+
+if [[ `hostname -s` = gray ]]; then 
+  /usr/bin/xrandr \
+    --output VGA-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output DVI-I-1 --mode 1920x1200 --pos 1920x0 --rotate normal \
+    --output HDMI-1 --off
+fi
+
