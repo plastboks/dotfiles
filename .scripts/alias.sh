bb5531f73569e18b12e1bb996a96e65da4f0e22b
Alexander Skjolden <alex@plastboks.net>
2012-12-26 14:43:45 +0100

    removed some non working aliases

--- .scripts/alias.sh
+++ .scripts/alias.sh
@@ -1,5 +1,2 @@
 # some nice aliases
 
-
-alias backup-mount='sshfs $user@$server: $path -o uid=$(id -u) -o gid=$(id -g)'
-alias pdfmerge='gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=./output.pdf'
