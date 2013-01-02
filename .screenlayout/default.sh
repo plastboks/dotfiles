244f7f4787fc3889fe1cc75f9b4321a06b46be76
Alexander Skjolden <alex@plastboks.net>
2013-01-02 19:10:37 +0100

    new gray xrandr

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -3,7 +3,7 @@
 # -- Gray -- #
 if [[ `hostname -s` = gray ]]; then 
   /usr/bin/xrandr \
-    --output VGA-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output VGA-1 --mode 1920x1080 --pos 0x120 --rotate normal \
     --output DVI-I-1 --mode 1920x1200 --pos 1920x0 --rotate normal \
     --output HDMI-1 --off
 fi
