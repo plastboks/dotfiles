f759461210d3ab725d7419bc2ad22d072991e4ec
Alexander Skjolden <alex@plastboks.net>
2013-01-02 19:02:35 +0100

    new lightgray xrandr layout

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -14,7 +14,7 @@ fi
 
 if [[ `hostname -s` = lightgray ]]; then
   /usr/bin/xrandr \
-    --output VGA-1 --mode 1920x1080 --pos 0x120 --rotate normal \
-    --output TV-1 --off \
-    --output DVI-I-1 --mode 1920x1200 --pos 1920x0 --rotate normal
+    --output VGA-1 --off \
+    --output TV-1 --off 
+    --output DVI-I-1 --mode 1680x1050 --pos 0x0 --rotate normal
 fi
