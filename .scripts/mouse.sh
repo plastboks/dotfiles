091266a87de7c34f4e0dfeab1a122cb145a11515
Alexander Skjolden <alex@plastboks.net>
2013-01-15 08:07:53 +0100

    alternative to scrolling

---
+++ .scripts/mouse.sh
@@ -0,0 +1,2 @@
+xinput set-int-prop "Kensington Kensington USB/PS2 Orbit" "Evdev Wheel Emulation Button" 8 1
+xinput set-int-prop "Kensington Kensington USB/PS2 Orbit" "Evdev Wheel Emulation Button" 8 3
