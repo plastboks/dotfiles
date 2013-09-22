6ffc2e2e67f24cb8038f706e381c62529110f2b9
Alexander Skjolden <alex@plastboks.net>
2013-09-22 11:20:39 +0200

    memory usage in i3status
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .i3/i3statuswrapper2.sh
+++ .i3/i3statuswrapper2.sh
@@ -4,11 +4,14 @@ i3status --config ~/.i3status.conf | while :
 do
     read line
     SL=$(xset -q|grep "Scroll Lock"| awk '{ print $12 }')
+    TM=$( free -m | grep "Mem" | awk '{print $2}')
+    UM=$( free -m | grep "buffers\/cache" | awk '{print $3}')
     if [ $SL == "on" ]
     then
-        dat="[{ \"full_text\": \"SL: $SL\", \"color\":\"#009E00\" },"
+        dat="[{ \"full_text\": \"SL: $SL\", \"color\":\"#FF8C00\" },"
     else
-        dat="[{ \"full_text\": \"SL: $SL\", \"color\":\"#C60101\" },"
+        dat="[{ \"full_text\": \"SL: $SL\", \"color\":\"#FF00FF\" },"
     fi
-    echo "${line/[/$dat}" || exit 1
+    MEM="{ \"full_text\": \"Mem: $TM\/$UM(MB)\", \"color\":\"#00FFFF\" },"
+    echo "${line/[/$dat $MEM}" || exit 1
 done
