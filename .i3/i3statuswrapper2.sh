e8ef6305b437ee54ee878ef33a4a746b6e0c56f7
Alexander Skjolden <alex@plastboks.net>
2013-09-22 17:47:51 +0200

    more accurate cpu temp
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .i3/i3statuswrapper2.sh
+++ .i3/i3statuswrapper2.sh
@@ -6,12 +6,14 @@ do
     SL=$(xset -q|grep "Scroll Lock"| awk '{ print $12 }')
     TM=$( free -m | grep "Mem" | awk '{print $2}')
     UM=$( free -m | grep "buffers\/cache" | awk '{print $3}')
+    CPUTEMP=$(sensors | grep "Core 0" | awk '{print $3}')
     if [ $SL == "on" ]
     then
         dat="[{ \"full_text\": \"SL: $SL\", \"color\":\"#FF8C00\" },"
     else
         dat="[{ \"full_text\": \"SL: $SL\", \"color\":\"#FF00FF\" },"
     fi
-    MEM="{ \"full_text\": \"Mem: $TM\/$UM(MB)\", \"color\":\"#00FFFF\" },"
-    echo "${line/[/$dat $MEM}" || exit 1
+    MEM="{ \"full_text\": \"MEM: $TM\/$UM(MB)\", \"color\":\"#00FFFF\" },"
+    CT="{ \"full_text\": \"CPU T: $CPUTEMP\", \"color\":\"#FFFF00\" },"
+    echo "${line/[/$dat $MEM $CT}" || exit 1
 done
