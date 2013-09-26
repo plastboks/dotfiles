2792d6b3166ce28e8c39535d749a0db93f7b5f7d
Alexander Skjolden <alex@plastboks.net>
2013-09-26 19:18:00 +0200

    some more fancystuff for i3status bar
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .i3/i3statuswrapper.sh
+++ .i3/i3statuswrapper.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
-# shell script to prepend i3status with more stuff
 
-i3status | while :
+i3status --config ~/.i3status.conf | while :
 do
-  read line
-  CURLAYOUT=`xset -q | grep -A 0 'LED' | cut -c59-67`
-  if [ $CURLAYOUT = "00000000" ]; then
-    CURLAYOUT="US"
-  fi
-  if [ $CURLAYOUT = "00001004" ]; then
-    CURLAYOUT="NO"
-  fi
-  echo "Layout: ${CURLAYOUT} | $line" || exit 1
+    read line
+    SL=$(xset -q|grep "Scroll Lock"| awk '{ print $12 }')
+    TM=$( free -m | grep "Mem" | awk '{print $2}')
+    UM=$( free -m | grep "buffers\/cache" | awk '{print $3}')
+    CPUTEMP=$(sensors | grep "Physical id" | awk '{print $4}')
+    CPUGOV=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+    CPUSPEED=$(grep "cpu MHz" /proc/cpuinfo | awk '{sum+=$4} END {printf "%04d", sum/NR}')
+    
+    CPUS="[{ \"full_text\": \"CPU S: $CPUSPEED MHz\", \"color\":\"#666666\" },"
+    if [ $SL == "on" ]
+    then
+        dat="{ \"full_text\": \"SL: $SL\", \"color\":\"#FF8C00\" },"
+    else
+        dat="{ \"full_text\": \"SL: $SL\", \"color\":\"#FF00FF\" },"
+    fi
+    MEM="{ \"full_text\": \"MEM: $UM\/$TM(MB)\", \"color\":\"#00FFFF\" },"
+    CT="{ \"full_text\": \"CPU T: $CPUTEMP\", \"color\":\"#FFFF00\" },"
+    CPUG="{ \"full_text\": \"CPU G: $CPUGOV\", \"color\":\"#7FFFG4\" },"
+    echo "${line/[/$CPUS $CPUG $dat $MEM $CT }" || exit 1
 done
