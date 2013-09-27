83fe23b9bd32bd981af2c4b596e008bd2a322aac
Alexander Skjolden <alex@plastboks.net>
2013-09-27 10:19:22 +0200

    more i3status info
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .i3/i3statuswrapper.sh
+++ .i3/i3statuswrapper.sh
@@ -6,19 +6,26 @@ do
     SL=$(xset -q|grep "Scroll Lock"| awk '{ print $12 }')
     TM=$( free -m | grep "Mem" | awk '{print $2}')
     UM=$( free -m | grep "buffers\/cache" | awk '{print $3}')
+    KER=$(uname -r)
+    CONDISK=$(grep -c "Vendor" /proc/scsi/scsi)
     CPUTEMP=$(sensors | grep "Physical id" | awk '{print $4}')
     CPUGOV=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
     CPUSPEED=$(grep "cpu MHz" /proc/cpuinfo | awk '{sum+=$4} END {printf "%04d", sum/NR}')
     
-    CPUS="[{ \"full_text\": \"CPU S: $CPUSPEED MHz\", \"color\":\"#666666\" },"
+    CPUS="[{ \"full_text\": \"C.S: $CPUSPEED MHz\", \"color\":\"#666666\" },"
+    CPUG="{ \"full_text\": \"C.G: $CPUGOV\", \"color\":\"#7FFFG4\" },"
+    CT="{ \"full_text\": \"C.T: $CPUTEMP\", \"color\":\"#FFFF00\" },"
+    MEM="{ \"full_text\": \"M: $UM\/$TM(MB)\", \"color\":\"#00FFFF\" },"
+    KERNEL="{ \"full_text\": \"$KER\", \"color\":\"#4D4DDC\" },"
+    CDISK="{ \"full_text\": \"Dsk: $CONDISK\", \"color\":\"#D2691E\" },"
+
     if [ $SL == "on" ]
     then
-        dat="{ \"full_text\": \"SL: $SL\", \"color\":\"#FF8C00\" },"
+        SRL="{ \"full_text\": \"SL: $SL\", \"color\":\"#FF8C00\" },"
     else
-        dat="{ \"full_text\": \"SL: $SL\", \"color\":\"#FF00FF\" },"
+        SRL="{ \"full_text\": \"SL: $SL\", \"color\":\"#FF00FF\" },"
     fi
-    MEM="{ \"full_text\": \"MEM: $UM\/$TM(MB)\", \"color\":\"#00FFFF\" },"
-    CT="{ \"full_text\": \"CPU T: $CPUTEMP\", \"color\":\"#FFFF00\" },"
-    CPUG="{ \"full_text\": \"CPU G: $CPUGOV\", \"color\":\"#7FFFG4\" },"
-    echo "${line/[/$CPUS $CPUG $dat $MEM $CT }" || exit 1
+
+
+    echo "${line/[/$CPUS $CPUG $CT $MEM $KERNEL $CDISK $SRL}" || exit 1
 done
