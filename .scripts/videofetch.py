f9a09c8c25f2fd587af8c707a4d962a2057b981e
Alexander Skjolden <alex@plastboks.net>
2013-10-08 21:51:20 +0200

    just to remember
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .scripts/videofetch.py
+++ .scripts/videofetch.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
-def tumbleURL(url, bitrate):
-  url = url.replace('/z/', '/i/', 1)
-  url = url.rsplit('/', 1)[0]
-  url = url + '/index_%s_av.m3u8' % bitrate
-  return url
+def fetchURL(vid):
+    return "http://nrk.no/serum/api/video/%s" % vid
 
+def tumbleURL(url, bitrate):
+    url = url.replace('/z/', '/i/', 1)
+    url = url.rsplit('/', 1)[0]
+    url = url + '/index_%s_av.m3u8' % bitrate
+    return url
