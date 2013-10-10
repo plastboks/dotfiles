c0844f253afdcb3e5c0f3cd7029da231f32b3ff2
Alexander Skjolden <alex@plastboks.net>
2013-10-10 12:53:31 +0200

    more safe with medium quality
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .scripts/webmplayer.sh
+++ .scripts/webmplayer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MOVIE=$(quvi dump -s good $1 | grep QUVI_MEDIA_STREAM_PROPERTY_URL= | sed -n 's/[^=]*\=//p') && mplayer $MOVIE
+MOVIE=$(quvi dump -s medium $1 | grep QUVI_MEDIA_STREAM_PROPERTY_URL= | sed -n 's/[^=]*\=//p') && mplayer $MOVIE
 
 #quvi dump -s medium -e "mplayer %u" $1
 
