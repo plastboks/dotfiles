76fc89bc30fe5f336d9a2ca38c5849692b031a97
Alexander Skjolden <alex@plastboks.net>
2014-06-06 08:40:01 +0200

    added tinygray with dual monitor setup
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -34,3 +34,11 @@ if [[ `hostname -s` = darkgray ]]; then
     --output HDMI1 --mode 1920x1200 --pos 1920x0 --rotate normal \
     --output VGA1 --off
 fi
+
+# -- TinyGray -- #
+if [[ `hostname -s` = tinygray ]]; then
+  /usr/bin/xrandr \
+    --output HDMI1 --mode 1920x1200 --pos 1920x0 --rotate normal \
+    --output VIRTUAL1 --off \
+    --output DP1 --off \
+    --output VGA1 --mode 1920x1080 --pos 0x120 --rotate normal
