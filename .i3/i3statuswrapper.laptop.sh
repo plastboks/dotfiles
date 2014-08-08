3aaa2ccd69b39f62fdfe632c12f74edf831b8c34
Alexander Skjolden <alex@plastboks.net>
2014-08-08 15:53:27 +0200

    fanspeed into i3statuswrapper
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .i3/i3statuswrapper.laptop.sh
+++ .i3/i3statuswrapper.laptop.sh
@@ -9,11 +9,13 @@ do
     KER=$(uname -r)
     CONDISK=$(grep -c "Vendor" /proc/scsi/scsi)
     CPUTEMP=$(sensors | grep "Physical id" | awk '{print $4}')
+    FANSPEED=$(sensors | grep "fan1" | awk '{print $2}')
     CPUGOV=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
     CPUSPEED=$(grep "cpu MHz" /proc/cpuinfo | awk '{sum+=$4} END {printf "%04d", sum/NR}')
 
     CPUS="[{ \"full_text\": \"$CPUSPEED MHz\", \"color\":\"#335C85\" },"
     CPUG="{ \"full_text\": \"C.G: $CPUGOV\", \"color\":\"#194775\" },"
+    FANS="{ \"full_text\": \"$FANSPEED RPM\", \"color\":\"#335C85\" },"
     CT="{ \"full_text\": \"$CPUTEMP\", \"color\":\"#335C85\" },"
     MEM="{ \"full_text\": \"$UM\/$TM(MB)\", \"color\":\"#194775\" },"
     KERNEL="{ \"full_text\": \"$KER\", \"color\":\"#335C85\" },"
@@ -27,5 +29,5 @@ do
     fi
 
 
-    echo "${line/[/$CPUS $CT $MEM $SRL}" || exit 1
+    echo "${line/[/$CPUS $FANS $CT $MEM $SRL}" || exit 1
 done
