9a98394642cd04115f8d9d43bceef00fc67b7d31
Alexander Skjolden <alex@plastboks.net>
2014-08-12 20:02:50 +0200

    nice git alias for fancy outputting of data
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -7,3 +7,6 @@
 alias ls='ls --color=auto'
 alias cpa='rsync -avzP'
 alias enable_alert='PS1="$PS1\a"'
+
+# alias for use with git
+alias lsd=log --graph --decorate --pretty=oneline --abbrev-commit --all
