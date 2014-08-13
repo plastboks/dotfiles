f757ced42042bf1548f9045c6d028e4fd495ae75
Alexander Skjolden <alex@plastboks.net>
2014-08-13 12:04:36 +0200

    nasty bug in aliases.sh files
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -9,4 +9,4 @@ alias cpa='rsync -avzP'
 alias enable_alert='PS1="$PS1\a"'
 
 # alias for use with git
-alias lsd=log --graph --decorate --pretty=oneline --abbrev-commit --all
+alias lsd='log --graph --decorate --pretty=oneline --abbrev-commit --all'
