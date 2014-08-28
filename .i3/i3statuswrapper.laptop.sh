529d94a601dccff116108cfd444eb1e120675a3a
Alexander Skjolden <alex@plastboks.net>
2014-08-28 12:01:25 +0200

    added backlight display to i3statuswrapper for laptop
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .i3/i3statuswrapper.laptop.sh
+++ .i3/i3statuswrapper.laptop.sh
@@ -18,10 +18,12 @@ do
     FANSPEED=$(sensors | grep "fan1" | awk '{print $2}')
     CPUGOV=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
     CPUSPEED=$(grep "cpu MHz" /proc/cpuinfo | awk '{sum+=$4} END {printf "%04d", sum/NR}')
+    BACKLIGHT=$(printf "%0.f" "$(xbacklight -get)")
 
     CPUS="[{ \"full_text\": \"$CPUSPEED MHz\", \"color\":\"$CHIG\" },"
     CPUG="{ \"full_text\": \"C.G: $CPUGOV\", \"color\":\"$CLOW\" },"
     FANS="{ \"full_text\": \"$FANSPEED RPM\", \"color\":\"$CLOW\" },"
+    BL="{ \"full_text\": \"BL: $BACKLIGHT\", \"color\":\"$CLOW\" },"
     CT="{ \"full_text\": \"$CPUTEMP\", \"color\":\"$CHIG\" },"
     MEM="{ \"full_text\": \"$UM\/$TM(MB)\", \"color\":\"$CLOW\" },"
     KERNEL="{ \"full_text\": \"$KER\", \"color\":\"$CHIG\" },"
@@ -35,5 +37,5 @@ do
     fi
 
 
-    echo "${line/[/$CPUS $FANS $CT $MEM $SRL}" || exit 1
+    echo "${line/[/$CPUS $FANS $CT $MEM $SRL $BL}" || exit 1
 done
