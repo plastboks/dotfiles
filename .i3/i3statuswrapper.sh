ee629591091d888dd57f26ab6310d119b9622a73
Alexander Skjolden <alex@plastboks.net>
2015-05-07 21:19:47 +0200

    variable names for colors in i3statuswrapper
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .i3/i3statuswrapper.sh
+++ .i3/i3statuswrapper.sh
@@ -3,6 +3,11 @@
 i3status --config ~/.i3status.conf | while :
 do
     read line
+
+    COLOR1="#668066"
+    COLOR2="#7A997A"
+    COLOR3="#B2248F"
+
     SL=$(xset -q|grep "Scroll Lock"| awk '{ print $12 }')
     TM=$( free -m | grep "Mem" | awk '{print $2}')
     UM=$( free -m | grep "Mem" | awk '{print $3}')
@@ -12,20 +17,19 @@ do
     CPUGOV=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
     CPUSPEED=$(grep "cpu MHz" /proc/cpuinfo | awk '{sum+=$4} END {printf "%04d", sum/NR}')
     
-    CPUS="[{ \"full_text\": \"C.S: $CPUSPEED MHz\", \"color\":\"#335C85\" },"
-    CPUG="{ \"full_text\": \"C.G: $CPUGOV\", \"color\":\"#194775\" },"
-    CT="{ \"full_text\": \"C.T: $CPUTEMP\", \"color\":\"#335C85\" },"
-    MEM="{ \"full_text\": \"M: $UM\/$TM(MB)\", \"color\":\"#194775\" },"
-    KERNEL="{ \"full_text\": \"$KER\", \"color\":\"#335C85\" },"
-    CDISK="{ \"full_text\": \"Dsk: $CONDISK\", \"color\":\"#194775\" },"
+    CPUS='[{ "full_text": "C.S: '$CPUSPEED' MHz", "color": "'$COLOR1'" },'
+    CPUG='{ "full_text": "C.G: '$CPUGOV'", "color": "'$COLOR2'" },'
+    CT='{ "full_text": "C.T: '$CPUTEMP'", "color": "'$COLOR1'" },'
+    MEM='{ "full_text": "M: '$UM'/'$TM'(MB)", "color": "'$COLOR2'" },'
+    KERNEL='{ "full_text": "'$KER'", "color": "'$COLOR1'" },'
+    CDISK='{ "full_text": "Dsk: '$CONDISK'", "color": "'$COLOR2'" },'
 
     if [ $SL == "on" ]
     then
-        SRL="{ \"full_text\": \"SL: $SL\", \"color\":\"#B2248F\" },"
+        SRL='{ "full_text": "SL: '$SL'", "color": "'$COLOR3'" },'
     else
-        SRL="{ \"full_text\": \"SL: $SL\", \"color\":\"#335C85\" },"
+        SRL='{ "full_text": "SL: '$SL'", "color": "'$COLOR1'" },'
     fi
-
 
     echo "${line/[/$CPUS $CPUG $CT $MEM $KERNEL $CDISK $SRL}" || exit 1
 done
