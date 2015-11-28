91ea8c1e0324431152c8f283198a474c4bd34dd8
Alexander Skjolden <alex@plastboks.net>
2015-11-28 21:03:49 +0100

    more typos - screenlayout
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -3,13 +3,12 @@
 # -- DarkGray -- #
 if [[ `hostname -s` = darkgray ]]; then
     /usr/bin/xrandr \
-        --output VIRTUAL1 \
-        --off --output DP3 \
-        --off --output DP2 \
-        --off --output DP1 \
-        --off --output HDMI3 \
-        --mode 1920x1200 --pos 3200x0 --rotate normal \
+        --output VIRTUAL1 --off \
+        --output DP3 --off \
+        --output DP2 --off \
+        --output DP1 --off \
+        --output HDMI3 --mode 1920x1200 --pos 3200x0 --rotate normal \
         --output HDMI2 --primary --mode 1920x1200 --pos 1280x0 --rotate normal \
-        --output HDMI1 --mode 1280x960 --pos 0x240 --rotate normal \
+        --output HDMI1 --mode 1280x720 --pos 0x472 --rotate normal \
         --output VGA1 --off
 fi
