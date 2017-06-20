4fe0f611070e7d81633ead3be6d441fddfbed374
Alexander Skjolden <alex@plastboks.net>
2017-06-20 13:41:07 +0200

    i3 adjustments for laptop

--- .i3/i3statuswrapper.laptop.sh
+++ .i3/i3statuswrapper.laptop.sh
@@ -17,10 +17,10 @@ do
     SL=$(xset -q|grep "Scroll Lock"| awk '{ print $12 }')
     TM=$( free -m | grep "Mem" | awk '{print $2}')
     UM=$( free -m | grep "Mem" | awk '{print $3}')
-    KER=$(uname -r)
-    CONDISK=$(grep -c "Vendor" /proc/scsi/scsi)
-    CPUTEMP=$(sensors | grep "Physical id" | awk '{print $4}')
-    FANSPEED=$(sensors | grep "fan1" | awk '{print $2}')
+    #KER=$(uname -r)
+    #CONDISK=$(grep -c "Vendor" /proc/scsi/scsi)
+    CPUTEMP=$(sensors coretemp-isa-0000 | grep "Physical id" | awk '{print $4}')
+    FANSPEED=$(sensors thinkpad-isa-0000 | grep "fan1" | awk '{print $2}')
     CPUGOV=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
     CPUSPEED=$(grep "cpu MHz" /proc/cpuinfo | awk '{sum+=$4} END {printf "%04d", sum/NR}')
     BACKLIGHT=$(printf "%0.f" "$(xbacklight -get)")
@@ -31,9 +31,8 @@ do
     CPUG='{ "full_text": "C.G: '$CPUGOV'", "color": "'$COLOR2'" },'
     CT='{ "full_text": "C.T: '$CPUTEMP'", "color": "'$COLOR1'" },'
     MEM='{ "full_text": "M: '$UM'/'$TM'(MB)", "color": "'$COLOR2'" },'
-    KERNEL='{ "full_text": "'$KER'", "color": "'$COLOR1'" },'
-    CDISK='{ "full_text": "Dsk: '$CONDISK'", "color": "'$COLOR2'" },'
-
+    #KERNEL='{ "full_text": "'$KER'", "color": "'$COLOR1'" },'
+    #CDISK='{ "full_text": "Dsk: '$CONDISK'", "color": "'$COLOR2'" },'
     FANS='{ "full_text": "'$FANSPEED' RPM", "color": "'$CLOW'" },'
     BL='{ "full_text": "BL: '$BACKLIGHT'", "color": "'$CLOW'" },'
    
@@ -44,5 +43,5 @@ do
         SRL='{ "full_text": "SL: '$SL'", "color": "'$COLOR1'" },'
     fi
 
-    echo "${line/[/ $START $CPUS $FANS $CT $MEM $SRL $BL}" || exit 1
+    echo "${line/[/ $START $CPUS $CPUG $FANS $CT $MEM $SRL $BL}" || exit 1
 done
