10af5a8951177404a7beba1b646b4f5fa32cfbca
Alexander Skjolden <alex@plastboks.net>
2018-07-08 14:38:53 +0200

    Added colors for texstudio tab

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-
-
-#stop keychain
-#/usr/bin/keychain -k all
-
-
-# take screen shot and distort it
-BG_FILE=/home/alex/.wallpaper.png
-#scrot $BG_FILE && convert $BG_FILE -scale 4% -scale 2500% $BG_FILE
-# activate locker
-/usr/bin/i3lock -p default -n -f -c 000000 #-i $BG_FILE
+/usr/bin/i3lock -p default -n -f -c 000000
